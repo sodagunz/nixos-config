9f805dfb67fcd93ef352a7b421fb209837dca5ee
tomasguinzburg <tomasguinzburg@gmail.com>
2025-06-28 22:26:58 +0200

    fix: adress more interpolation damage

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -5,16 +5,9 @@
     themeFile = "gruvbox-dark-hard";
 
     font = {
-      name = "Maple Mono";
+      name = "DejaVu Sans Mono";
       size = 16;
     };
-
-    extraConfig = ''
-      font_features MapleMono-Regular +ss01 +ss02 +ss04
-      font_features MapleMono-Bold +ss01 +ss02 +ss04
-      font_features MapleMono-Italic +ss01 +ss02 +ss04
-      font_features MapleMono-Light +ss01 +ss02 +ss04
-    '';
 
     settings = {
       confirm_os_window_close = 0;
--- modules/home/waybar/style.nix
+++ modules/home/waybar/style.nix
@@ -2,25 +2,6 @@
   config,
   pkgs,
   ...
-}: let
-  custom = {
-    font = "Maple Mono";
-    font_size = "18px";
-    font_weight = "bold";
-    text_color = "#${config.colorScheme.palette.base06}";
-    background_0 = "#${config.colorScheme.palette.base00}";
-    background_1 = "#${config.colorScheme.palette.base02}";
-    border_color = "#${config.colorScheme.palette.base01}";
-    red = "#${config.colorScheme.palette.base08}";
-    green = "#${config.colorScheme.palette.base0C}";
-    yellow = "#${config.colorScheme.palette.base0A}";
-    blue = "#${config.colorScheme.palette.base0D}";
-    magenta = "#${config.colorScheme.palette.base0E}";
-    cyan = "#${config.colorScheme.palette.base0B}";
-    orange = "#${config.colorScheme.palette.base0F}";
-    opacity = "1";
-    indicator_height = "2px";
-  };
-in {
-  programs.waybar.style = with custom; (pkgs.lib.strings.fileContents ./style.css);
+}: {
+  programs.waybar.style = (pkgs.lib.strings.fileContents ./style.css);
 }
