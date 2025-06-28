6a8aeac4429731326d51a807ce529fe4802dd1a3
tomasguinzburg <tomasguinzburg@gmail.com>
2025-06-28 21:44:17 +0200

    fix: rofi theme interpolation

--- modules/home/rofi/default.nix
+++ modules/home/rofi/default.nix
@@ -5,7 +5,19 @@
 }: {
   home.packages = with pkgs; [rofi-wayland];
 
-  xdg.configFile."rofi/theme.rasi".source = ./theme.rasi;
+  xdg.configFile."rofi/theme.rasi".text = ''
+    * {
+      bg-col: #${config.colorScheme.palette.base00};
+      bg-col-light: #${config.colorScheme.palette.base02};
+      border-col: #${config.colorScheme.palette.base01};
+      selected-col: #${config.colorScheme.palette.base03};
+      cyan: #${config.colorScheme.palette.base0C};
+      fg-col: #${config.colorScheme.palette.base07};
+      fg-col2: #${config.colorScheme.palette.base06};
+      grey: #${config.colorScheme.palette.base05};
+      highlight: @cyan;
+    }
+  '';
 
   xdg.configFile."rofi/config.rasi".source = ./config.rasi;
 }
