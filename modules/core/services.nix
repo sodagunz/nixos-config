ce860e0e2617a90de477cba2fc31759bf99ff7ef
tomasguinzburg <tomasguinzburg@gmail.com>
2025-06-30 19:19:30 +0200

    feat: openssh

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -4,6 +4,7 @@
     gnome.gnome-keyring.enable = true;
     dbus.enable = true;
     fstrim.enable = true;
+    openssh.enable = true;
   };
   services.logind.extraConfig = ''
     # don’t shutdown when power button is short-pressed
