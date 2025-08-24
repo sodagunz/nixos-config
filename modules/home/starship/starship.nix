546d382ad724e4fcea3192aeaf364df68da40d21
tomasguinzburg <tomasguinzburg@gmail.com>
2025-08-24 15:14:04 +0200

    feat: starship zsh and bash

--- modules/home/starship/starship.nix
+++ modules/home/starship/starship.nix
@@ -5,6 +5,8 @@
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
     enableTransience = true;
   };
 }
