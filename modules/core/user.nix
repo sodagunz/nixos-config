328e3388753cf453f11083b39403db47cad9836e
tomasguinzburg <tomasguinzburg@gmail.com>
2025-10-12 11:37:15 +0200

    feat: add win pub ssh

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -37,6 +37,7 @@
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBOPsjlsehCwuVVDsyaKJoxvIefqu1BPl2/B3V8dghrf gunz"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBhO59Hku23ejz5cyWdXQRnk8TpecPNAbZZw0VD60bv tomas"
     ];
   };
   nix.settings.allowed-users = ["${username}"];
