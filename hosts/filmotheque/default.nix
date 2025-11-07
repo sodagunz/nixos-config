66c2cf518d141fbed53cf6d42ad1ad9872ad89ae
sodagunz <sodagunz@proton.me>
2025-11-07 12:07:03 +0100

    feat(filmotheque): zfs + disks

--- hosts/filmotheque/default.nix
+++ hosts/filmotheque/default.nix
@@ -1,4 +1,4 @@
-_: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
     ./../../modules/core/default.server.nix
@@ -6,4 +6,48 @@ _: {
 
   powerManagement.cpuFreqGovernor = "schedutil";
   programs.ssh.startAgent = true;
+
+  boot = {
+    supportedFilesystems = ["zfs"];
+    zfs = {
+      forceImportRoot = false;
+      extraPools = ["tank"];
+      package = pkgs.zfs;
+      devNodes = "/dev/disk/by-id";
+    };
+    # ZFS usually lags behind latest
+    kernelPackages = pkgs.linuxPackages;
+  };
+  services.zfs = {
+    autoScrub = {
+      enable = true;
+      interval = "weekly";
+    };
+
+
+    autoSnapshot.enable = false;
+  };
+
+
+  # This should be an 8 character hex, you can get it via
+  # head -c4 /dev/urandom | od -A none -t x4
+  # It should be unique, and static between builds.
+  networking.hostId = "83a73ad2";
+
+  services.smartd = {
+    enable = true;
+    autodetect = true;
+    devices = [
+      {
+        device = "/dev/disk/by-id/ata-TOSHIBA_MG08ACA16TE_51S0A6CMFVGG";
+        options = "-a -o on -S on -s S(/../.././02|L/../../7/03)";
+      }
+      {
+        device = "/dev/disk/by-id/ata-TOSHIBA_MG08ACA16TE_51S0A6X5FVGG";
+        options = "-a -o on -S on -s S(/../.././02|L/../../7/03)";
+      }
+    ];
+  };
+
+  environment.systemPackages = [pkgs.smartmontools];
 }
