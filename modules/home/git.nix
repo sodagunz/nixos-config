b06f9571737cb049da0044811fab7fa73441131b
sodagunz <sodagunz@proton.me>
2025-11-07 18:08:55 +0100

    chore: update git config

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,20 +1,50 @@
 {pkgs, ...}: {
-  programs.git = {
-    enable = true;
+  programs = {
+    git = {
+      enable = true;
+      settings = {
+        user = {
+          name = "sodagunz";
+          email = "sodagunz@proton.me";
+        };
+        init.defaultBranch = "main";
+        credential.helper = "store";
+        merge.conflictstyle = "diff3";
+        diff.colorMoved = "default";
+        push.autoSetupRemote = true;
+      };
+    }; # pkgs.git-lfs
 
-    userName = "sodagunz";
-    userEmail = "sodagunz@proton.me";
-
-    extraConfig = {
-      init.defaultBranch = "main";
-      credential.helper = "store";
-      merge.conflictstyle = "diff3";
-      diff.colorMoved = "default";
-      push.autoSetupRemote = true;
+    zsh.shellAliases = {
+      g = "lazygit";
+      gf = "onefetch --number-of-file-churns 0 --no-color-palette";
+      ga = "git add";
+      gaa = "git add --all";
+      gs = "git status";
+      gb = "git branch";
+      gm = "git merge";
+      gd = "git diff";
+      gpl = "git pull";
+      gplo = "git pull origin";
+      gps = "git push";
+      gpso = "git push origin";
+      gpst = "git push --follow-tags";
+      gcl = "git clone";
+      gc = "git commit";
+      gcm = "git commit -m";
+      gcma = "git add --all && git commit -m";
+      gtag = "git tag -ma";
+      gch = "git checkout";
+      gchb = "git checkout -b";
+      glog = "git log --oneline --decorate --graph";
+      glol = "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'";
+      glola = "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --all";
+      glols = "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --stat";
     };
 
     delta = {
       enable = true;
+      enableGitIntegration = true;
       options = {
         line-numbers = true;
         side-by-side = true;
@@ -24,32 +54,5 @@
     };
   };
 
-  home.packages = [pkgs.gh]; # pkgs.git-lfs
-
-  programs.zsh.shellAliases = {
-    g = "lazygit";
-    gf = "onefetch --number-of-file-churns 0 --no-color-palette";
-    ga = "git add";
-    gaa = "git add --all";
-    gs = "git status";
-    gb = "git branch";
-    gm = "git merge";
-    gd = "git diff";
-    gpl = "git pull";
-    gplo = "git pull origin";
-    gps = "git push";
-    gpso = "git push origin";
-    gpst = "git push --follow-tags";
-    gcl = "git clone";
-    gc = "git commit";
-    gcm = "git commit -m";
-    gcma = "git add --all && git commit -m";
-    gtag = "git tag -ma";
-    gch = "git checkout";
-    gchb = "git checkout -b";
-    glog = "git log --oneline --decorate --graph";
-    glol = "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'";
-    glola = "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --all";
-    glols = "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --stat";
-  };
+  home.packages = [pkgs.gh];
 }
