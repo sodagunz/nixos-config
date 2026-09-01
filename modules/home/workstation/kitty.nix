df12f77fecbe549e9219956fdf4709371d5d1218
sodagunz <sodagunz@proton.me>
2026-09-01 21:32:56 +0200

    feat(kitty): fix transparency

--- modules/home/workstation/kitty.nix
+++ modules/home/workstation/kitty.nix
@@ -11,7 +11,7 @@ _: {
 
     settings = {
       confirm_os_window_close = 0;
-      background_opacity = "0.66";
+      background_opacity = "1";
       scrollback_lines = 10000;
       enable_audio_bell = false;
       mouse_hide_wait = 60;
